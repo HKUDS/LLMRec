1b81060c3d53a8039f87a2530ea799bb9e01e4be
mohamedaminebentayeb <jm_bentayeb@esi.dz>
2024-05-09 14:46:56 +0200

    comment added

--- Models.py
+++ Models.py
@@ -12,13 +12,14 @@ from torch.nn import init
 
 from utility.parser import parse_args
 from utility.norm import build_sim, build_knn_normalized_graph
+#parse the arguments
 args = parse_args()
-
+#setting the device
 device = torch.device("cpu")
-
+#defining the MM_Model class
 class MM_Model(nn.Module):
     def __init__(self, n_users, n_items, embedding_dim, weight_size, dropout_list, image_feats, text_feats, user_init_embedding, item_attribute_dict):
-
+        #init the model
         super().__init__()
         self.n_users = n_users
         self.n_items = n_items
@@ -44,6 +45,7 @@ class MM_Model(nn.Module):
         self.text_feats = torch.tensor(text_feats).float()
         self.user_feats = torch.tensor(user_init_embedding).float()
         self.item_feats = {}
+        # item attribute
         for key in item_attribute_dict.keys():                                   
             self.item_feats[key] = torch.tensor(item_attribute_dict[key]).float() 
 
@@ -53,30 +55,34 @@ class MM_Model(nn.Module):
         self.dropout = nn.Dropout(p=args.drop_rate)
         self.batch_norm = nn.BatchNorm1d(args.embed_size)
         self.tau = 0.5
-
+    #defining the forward pass
     def mm(self, x, y):
         if args.sparse:
             return torch.sparse.mm(x, y)
         else:
             return torch.mm(x, y)
+    #defining the similarity function
     def sim(self, z1, z2):
         z1 = F.normalize(z1)
         z2 = F.normalize(z2)
         return torch.mm(z1, z2.t())
-
+    #defining the contrastive loss function
     def batched_contrastive_loss(self, z1, z2, batch_size=4096):
+        #batched contrastive loss
         device = z1.device
         num_nodes = z1.size(0)
         num_batches = (num_nodes - 1) // batch_size + 1
         f = lambda x: torch.exp(x / self.tau)
         indices = torch.arange(0, num_nodes).to(device)
         losses = []
-
+        #for each batch
         for i in range(num_batches):
+            #get the mask
             mask = indices[i * batch_size:(i + 1) * batch_size]
+            #compute the similarity
             refl_sim = f(self.sim(z1[mask], z1))  
             between_sim = f(self.sim(z1[mask], z2))  
-
+            #compute the loss
             losses.append(-torch.log(
                 between_sim[:, i * batch_size:(i + 1) * batch_size].diag()
                 / (refl_sim.sum(1) + between_sim.sum(1)
@@ -84,24 +90,26 @@ class MM_Model(nn.Module):
                    
         loss_vec = torch.cat(losses)
         return loss_vec.mean()
-
+    #defininig csr_norm function
     def csr_norm(self, csr_mat, mean_flag=False):
+        #csr matrix normalization
         rowsum = np.array(csr_mat.sum(1))
         rowsum = np.power(rowsum+1e-8, -0.5).flatten()
         rowsum[np.isinf(rowsum)] = 0.
         rowsum_diag = sp.diags(rowsum)
-
+        #column sum
         colsum = np.array(csr_mat.sum(0))
         colsum = np.power(colsum+1e-8, -0.5).flatten()
         colsum[np.isinf(colsum)] = 0.
         colsum_diag = sp.diags(colsum)
-
+        #return the normalized matrix
         if mean_flag == False:
             return rowsum_diag*csr_mat*colsum_diag
         else:
             return rowsum_diag*csr_mat
-
+    #defining the matrix to tensor function
     def matrix_to_tensor(self, cur_matrix):
+        #convert the matrix to tensor
         if type(cur_matrix) != sp.coo_matrix:
             cur_matrix = cur_matrix.tocoo()  #
         indices = torch.from_numpy(np.vstack((cur_matrix.row, cur_matrix.col)).astype(np.int64))  #
@@ -109,21 +117,22 @@ class MM_Model(nn.Module):
         shape = torch.Size(cur_matrix.shape)
 
         return torch.sparse.FloatTensor(indices, values, shape).to(torch.float32)  #
-
+    #defining the forward pass
     def para_dict_to_tenser(self, para_dict):  
         """
         :param para_dict: nn.ParameterDict()
         :return: tensor
         """
         tensors = []
-
+        #for each behavior
         for beh in para_dict.keys():
+            #append the tensor
             tensors.append(para_dict[beh])
         tensors = torch.stack(tensors, dim=0)
 
         return tensors
 
-
+    #defining the forward pass
     def forward(self, ui_graph, iu_graph, image_ui_graph, image_iu_graph, text_ui_graph, text_iu_graph):
 
 
@@ -135,20 +144,22 @@ class MM_Model(nn.Module):
             i_mask_nodes = i_perm[: i_num_mask_nodes]
             for key in self.item_feats.keys():
                 self.item_feats[key][i_mask_nodes] = self.item_feats[key].mean(0)
-
+        # user mask
         u_perm = torch.randperm(self.n_users)
         u_num_mask_nodes = int(args.mask_rate * self.n_users)
         u_mask_nodes = u_perm[: u_num_mask_nodes]
+        
         self.user_feats[u_mask_nodes] = self.user_feats.mean(0) 
 
-
+        # feature transformation
         image_feats = self.dropout(self.image_trans(self.image_feats))
         text_feats = self.dropout(self.text_trans(self.text_feats))
         user_feats = self.dropout(self.user_trans(self.user_feats.to(torch.float32)))
         item_feats = {}
+        # item attribute
         for key in self.item_feats.keys():
             item_feats[key] = self.dropout(self.item_trans(self.item_feats[key]))
-
+        # graph propagation
         for i in range(args.layers):
             image_user_feats = self.mm(ui_graph, image_feats)
             image_item_feats = self.mm(iu_graph, image_user_feats)
@@ -171,6 +182,7 @@ class MM_Model(nn.Module):
 
         user_emb_list = [u_g_embeddings]
         item_emb_list = [i_g_embeddings]
+        # graph propagation
         for i in range(self.n_ui_layers):    
             if i == (self.n_ui_layers-1):
                 u_g_embeddings = self.softmax( torch.mm(ui_graph, i_g_embeddings) ) 
@@ -181,7 +193,7 @@ class MM_Model(nn.Module):
 
             user_emb_list.append(u_g_embeddings)
             item_emb_list.append(i_g_embeddings)
-
+        # final embeddings
         u_g_embeddings = torch.mean(torch.stack(user_emb_list), dim=0)
         i_g_embeddings = torch.mean(torch.stack(item_emb_list), dim=0) 
 
@@ -198,10 +210,11 @@ class MM_Model(nn.Module):
 
         return u_g_embeddings, i_g_embeddings, image_item_feats, text_item_feats, image_user_feats, text_user_feats, user_feats, item_feats, user_prof_feat, item_prof_feat, user_feat_from_item, item_feats, i_mask_nodes, u_mask_nodes
 
-
+#defining the decoder class
 
 class Decoder(nn.Module):
     def __init__(self, feat_size):
+        #init the decoder
         super(Decoder, self).__init__()
         self.feat_size=feat_size
 
@@ -214,13 +227,17 @@ class Decoder(nn.Module):
             nn.Linear(args.embed_size, int(self.feat_size)),
             nn.LeakyReLU(True),
         )
-
+    #defining the forward pass
     def forward(self, u, i):
+        #forward pass
         u_output = self.u_net(u.float())  
         tensor_list = []
-        for index,value in enumerate(i.keys()):  
+        #for each index and value
+        for index,value in enumerate(i.keys()):
+            #append the tensor  
             tensor_list.append(i[value]) 
         i_tensor = torch.stack(tensor_list)
+        #forward pass
         i_output = self.i_net(i_tensor.float())  
         return u_output, i_output  
 
--- main.py
+++ main.py
@@ -30,18 +30,17 @@ from utility.logging import Logger
 from utility.norm import build_sim, build_knn_normalized_graph
 
 import setproctitle
-
+#get the arguments
 args = parse_args()
-
-print("Program started")
+#define trainer class
 class Trainer(object):
     def __init__(self, data_config):
-       
+        # set process name
         self.task_name = "%s_%s_%s" % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), args.dataset, args.cf_model,)
         self.logger = Logger(filename=self.task_name, is_debug=args.debug)
         self.logger.logging("PID: %d" % os.getpid())
         self.logger.logging(str(args))
-
+        #initialize data generator
         self.mess_dropout = eval(args.mess_dropout)
         self.lr = args.lr
         self.emb_dim = args.embed_size
@@ -50,7 +49,6 @@ class Trainer(object):
         self.n_layers = len(self.weight_size)
         self.regs = eval(args.regs)
         self.decay = self.regs[0]
- 
         self.image_feats = np.load(args.data_path + '{}/image_feat.npy'.format(args.dataset))
         self.text_feats = np.load(args.data_path + '{}/text_feat.npy'.format(args.dataset))
         self.image_feat_dim = self.image_feats.shape[-1]
@@ -60,6 +58,7 @@ class Trainer(object):
         # get user embedding  
         augmented_user_init_embedding = pickle.load(open(args.data_path + args.dataset + '/augmented_user_init_embedding','rb'))
         augmented_user_init_embedding_list = []
+        # get user embedding
         for i in range(len(augmented_user_init_embedding)):
             augmented_user_init_embedding_list.append(augmented_user_init_embedding[i])
         augmented_user_init_embedding_final = np.array(augmented_user_init_embedding_list)
@@ -72,9 +71,8 @@ class Trainer(object):
         else:
             print("Dealing with Netflix")
             augmented_total_embed_dict = {'year':[] , 'title':[], 'director':[], 'country':[], 'language':[]}   
-        print("Loading Pickle...")
         augmented_atttribute_embedding_dict = pickle.load(open(args.data_path + args.dataset + '/augmented_atttribute_embedding_dict','rb'))
-        print("Finished Loading Pickle.")
+        # get attribute embedding
         for value in augmented_atttribute_embedding_dict.keys():
             for i in range(len(augmented_atttribute_embedding_dict[value])):
                 augmented_total_embed_dict[value].append(augmented_atttribute_embedding_dict[value][i])   
@@ -84,22 +82,24 @@ class Trainer(object):
 
         self.image_ui_index = {'x':[], 'y':[]}
         self.text_ui_index = {'x':[], 'y':[]}
-
+        # get image and text index
         self.n_users = self.ui_graph.shape[0]
         self.n_items = self.ui_graph.shape[1]        
         self.iu_graph = self.ui_graph.T
-  
+        # normalize
         self.ui_graph = self.csr_norm(self.ui_graph, mean_flag=True)
         self.iu_graph = self.csr_norm(self.iu_graph, mean_flag=True)
+        # convert to tensor
         self.ui_graph = self.matrix_to_tensor(self.ui_graph)
         self.iu_graph = self.matrix_to_tensor(self.iu_graph)
+        #initialize graph
         self.image_ui_graph = self.text_ui_graph = self.ui_graph
         self.image_iu_graph = self.text_iu_graph = self.iu_graph
-        print("L3aliiiiiiiiiiiiiiiiiittttttttttttttttttttttttt")
+        #initialize model
         self.model_mm = MM_Model(self.n_users, self.n_items, self.emb_dim, self.weight_size, self.mess_dropout, self.image_feats, self.text_feats, self.user_init_embedding, self.item_attribute_embedding)      
         self.model_mm = self.model_mm  
         self.decoder = Decoder(self.user_init_embedding.shape[1])
-
+        #initialize optimizer
 
         self.optimizer = optim.AdamW(
         [
@@ -113,9 +113,9 @@ class Trainer(object):
         ]
             , lr=args.de_lr)  
 
-        print("Tekfa init")
-
+    #def csr_norm(self, csr_mat, mean_flag=False):
     def csr_norm(self, csr_mat, mean_flag=False):
+        
         rowsum = np.array(csr_mat.sum(1))
         rowsum = np.power(rowsum+1e-8, -0.5).flatten()
         rowsum[np.isinf(rowsum)] = 0.
@@ -128,37 +128,38 @@ class Trainer(object):
             return rowsum_diag*csr_mat*colsum_diag
         else:
             return rowsum_diag*csr_mat
-
+    #def matrix_to_tensor(self, cur_matrix):
     def matrix_to_tensor(self, cur_matrix):
+        # convert matrix to tensor
         if type(cur_matrix) != sp.coo_matrix:
             cur_matrix = cur_matrix.tocoo()  #
         indices = torch.from_numpy(np.vstack((cur_matrix.row, cur_matrix.col)).astype(np.int64))  #
         values = torch.from_numpy(cur_matrix.data)  #
         shape = torch.Size(cur_matrix.shape)
         return torch.sparse.FloatTensor(indices, values, shape).to(torch.float32)  #
-
+    #def innerProduct(self, u_pos, i_pos, u_neg, j_neg):
     def innerProduct(self, u_pos, i_pos, u_neg, j_neg):  
         pred_i = torch.sum(torch.mul(u_pos,i_pos), dim=-1) 
         pred_j = torch.sum(torch.mul(u_neg,j_neg), dim=-1)  
         return pred_i, pred_j
-
+    #def weights_init(self, m):
     def weights_init(self, m):
         if isinstance(m, nn.Linear):
             nn.init.kaiming_normal_(m.weight)
             m.bias.data.fill_(0)
-
+    #ef sim(self, z1, z2):
     def sim(self, z1, z2):
         z1 = F.normalize(z1)  
         z2 = F.normalize(z2)
         return torch.mm(z1, z2.t())
-
+    #def feat_reg_loss_calculation(self, g_item_image, g_item_text, g_user_image, g_user_text):
     def feat_reg_loss_calculation(self, g_item_image, g_item_text, g_user_image, g_user_text):
         feat_reg = 1./2*(g_item_image**2).sum() + 1./2*(g_item_text**2).sum() \
             + 1./2*(g_user_image**2).sum() + 1./2*(g_user_text**2).sum()        
         feat_reg = feat_reg / self.n_items
         feat_emb_loss = args.feat_reg_decay * feat_reg
         return feat_emb_loss
-
+    #def prune_loss(self, pred, drop_rate):
     def prune_loss(self, pred, drop_rate):
         ind_sorted = np.argsort(pred.cpu().data)
         loss_sorted = pred[ind_sorted]
@@ -167,7 +168,7 @@ class Trainer(object):
         ind_update = ind_sorted[:num_remember]
         loss_update = pred[ind_update]
         return loss_update.mean()
-
+    #def mse_criterion(self, x, y, alpha=3):
     def mse_criterion(self, x, y, alpha=3):
         x = F.normalize(x, p=2, dim=-1)
         y = F.normalize(y, p=2, dim=-1)
@@ -175,21 +176,21 @@ class Trainer(object):
         tmp_loss = tmp_loss.mean()
         loss = F.mse_loss(x, y)
         return loss
-
+    #def sce_criterion(self, x, y, alpha=1):
     def sce_criterion(self, x, y, alpha=1):
         x = F.normalize(x, p=2, dim=-1)
         y = F.normalize(y, p=2, dim=-1)
         loss = (1-(x*y).sum(dim=-1)).pow_(alpha)
         loss = loss.mean() 
         return loss
-
+    #def test(self, users_to_test, is_val):
     def test(self, users_to_test, is_val):
         self.model_mm.eval()
         with torch.no_grad():
             ua_embeddings, ia_embeddings, *rest = self.model_mm(self.ui_graph, self.iu_graph, self.image_ui_graph, self.image_iu_graph, self.text_ui_graph, self.text_iu_graph)
         result = test_torch(ua_embeddings, ia_embeddings, users_to_test, is_val)
         return result
-
+    #def test(self, users_to_test, is_val):
     def train(self):
         print("training")
         now_time = datetime.now()
@@ -197,9 +198,10 @@ class Trainer(object):
 
         training_time_list = []
         stopping_step = 0
-
+        # get the number of batches
         n_batch = data_generator.n_train // args.batch_size + 1
         best_recall = 0
+        # training
         for epoch in range(args.epoch):
             print("training epoches")
             t1 = time()
@@ -211,7 +213,7 @@ class Trainer(object):
 
             self.gene_u, self.gene_real, self.gene_fake = None, None, {}
             self.topk_p_dict, self.topk_id_dict = {}, {}
-
+            #for idx in range(n_batch):
             for idx in tqdm(range(n_batch)):
                 self.model_mm.train()
                 sample_t1 = time()
@@ -288,11 +290,11 @@ class Trainer(object):
                 reg_loss += float(batch_reg_loss)
     
             del user_presentation_h, item_presentation_h, u_bpr_emb, i_bpr_neg_emb, i_bpr_pos_emb
-
+            
             if math.isnan(loss) == True:
                 self.logger.logging('ERROR: loss is nan.')
                 sys.exit()
-
+            # print the results
             if (epoch + 1) % args.verbose != 0:
                 perf_str = 'Epoch %d [%.1fs]: train==[%.5f=%.5f + %.5f + %.5f  + %.5f]' % (
                     epoch, time() - t1, loss, mf_loss, emb_loss, reg_loss, contrastive_loss)
@@ -306,7 +308,7 @@ class Trainer(object):
             training_time_list.append(t2 - t1)
 
             t3 = time()
-
+            #if args.verbose > 0: then print the results
             if args.verbose > 0:
                 perf_str = 'Epoch %d [%.1fs + %.1fs]: train==[%.5f=%.5f + %.5f + %.5f], recall=[%.5f, %.5f, %.5f, %.5f], ' \
                            'precision=[%.5f, %.5f, %.5f, %.5f], hit=[%.5f, %.5f, %.5f, %.5f], ndcg=[%.5f, %.5f, %.5f, %.5f]' % \
@@ -315,12 +317,13 @@ class Trainer(object):
                             ret['precision'][0], ret['precision'][1], ret['precision'][2], ret['precision'][-1], ret['hit_ratio'][0], ret['hit_ratio'][1], ret['hit_ratio'][2], ret['hit_ratio'][-1],
                             ret['ndcg'][0], ret['ndcg'][1], ret['ndcg'][2], ret['ndcg'][-1])
                 self.logger.logging(perf_str)
-
+            # check whether recall is the best
             if ret['recall'][1] > best_recall:
                 best_recall = ret['recall'][1]
                 test_ret = self.test(users_to_test, is_val=False)
                 self.logger.logging("Test_Recall@%d: %.5f,  precision=[%.5f], ndcg=[%.5f]" % (eval(args.Ks)[1], test_ret['recall'][1], test_ret['precision'][1], test_ret['ndcg'][1]))
                 stopping_step = 0
+            # early stopping
             elif stopping_step < args.early_stopping_patience:
                 stopping_step += 1
                 self.logger.logging('#####Early stopping steps: %d #####' % stopping_step)
@@ -331,14 +334,15 @@ class Trainer(object):
 
         return best_recall, run_time 
 
-
+    #def bpr_loss(self, users, pos_items, neg_items):
     def bpr_loss(self, users, pos_items, neg_items):
+        # BPR loss
         pos_scores = torch.sum(torch.mul(users, pos_items), dim=1)
         neg_scores = torch.sum(torch.mul(users, neg_items), dim=1)
-
+        # regularizer
         regularizer = 1./(2*(users**2).sum()+1e-8) + 1./(2*(pos_items**2).sum()+1e-8) + 1./(2*(neg_items**2).sum()+1e-8)        
         regularizer = regularizer / self.batch_size
-
+        #calculate the loss
         maxi = F.logsigmoid(pos_scores - neg_scores+1e-8)
         mf_loss = - self.prune_loss(maxi, args.prune_loss_drop_rate)
 
@@ -346,7 +350,7 @@ class Trainer(object):
         reg_loss = 0.0
         return mf_loss, emb_loss, reg_loss
     
-
+    #def test(self, users_to_test, is_val):
     def sparse_mx_to_torch_sparse_tensor(self, sparse_mx):
         """Convert a scipy sparse matrix to a torch sparse tensor."""
         sparse_mx = sparse_mx.tocoo().astype(np.float32)
@@ -356,7 +360,7 @@ class Trainer(object):
         shape = torch.Size(sparse_mx.shape)
         return torch.sparse.FloatTensor(indices, values, shape)
     
-
+# set seed
 def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
@@ -364,13 +368,16 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)  
 
 if __name__ == '__main__':
+    # set cuda environment visible device
     os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu_id)
     set_seed(args.seed)
+    # initialize dataset
     config = dict()
     config['n_users'] = data_generator.n_users
     config['n_items'] = data_generator.n_items
-
+    # initialize trainer
     trainer = Trainer(data_config=config)
+    # training
     trainer.train()
 
 
